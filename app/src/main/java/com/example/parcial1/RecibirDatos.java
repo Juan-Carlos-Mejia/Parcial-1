12f46b8ad2a355a23ebeca0b8cbb193939e4e936
Juan-Carlos-Mejía <usis002221@ugb.edu.sv>
2022-03-04 18:15:59 -0600

    parcial 1

--- app/src/main/java/com/example/parcial1/RecibirDatos.java
+++ app/src/main/java/com/example/parcial1/RecibirDatos.java
@@ -2,6 +2,7 @@ package com.example.parcial1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,7 @@ public class RecibirDatos extends AppCompatActivity {
 
     Integer estado;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
